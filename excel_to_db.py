df7c6faff93b5d1fe3d0ef111a0bc55129463f1a
İREM NUR YILDIRIM <irem.yildirim1@boun.edu.tr>
2024-05-05 21:07:05 +0300

    excel to db script and removed unncessry entities

---
+++ excel_to_db.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+# Define your database connection string
+DATABASE_URI = 'mysql://root:password@127.0.0.1:3308/db'
+
+# Read the Excel file into a pandas DataFrame
+df = pd.read_excel('Sample Data - Project3.xlsx', sheet_name='SessionSquads')
+
+# Connect to your database using SQLAlchemy
+engine = create_engine(DATABASE_URI)
+
+# Insert the DataFrame into the database
+df.to_sql('SessionSquads', con=engine, if_exists='append', index=False)
